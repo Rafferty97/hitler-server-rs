228b8184427abb13d1bd6a449a0d56df02cd1d0e
Alexander Rafferty <alexander.drafferty@gmail.com>
2023-01-02 16:26:23 +0800

    Export to PG

--- src/pg.rs
+++ src/pg.rs
@@ -1,12 +1,102 @@
-use std::error::Error;
-use tokio_postgres::NoTls;
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use sled::IVec;
+use std::{error::Error, time::Duration};
+use tokio_postgres::{types::ToSql, Client, NoTls, Statement};
 
-pub async fn foo() -> Result<(), Box<dyn Error>> {
-    let (client, connection) = tokio_postgres::connect(
-        "host=secrethitler.live user=server password=5@+Yx6@)kFySy2Q4 dbname=secrethitler",
-        NoTls,
-    )
-    .await?;
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct GameStats {
+    pub id: String,
+    pub players: Vec<String>,
+    pub started: DateTime<Utc>,
+    pub finished: DateTime<Utc>,
+    pub outcome: Outcome,
+}
+
+#[derive(Clone, Copy, Serialize, Deserialize, Debug)]
+pub enum Outcome {
+    /// The liberals completed their policy track.
+    LiberalPolicyTrack,
+    /// The fascists completed their policy track.
+    FascistPolicyTrack,
+    /// The communists completed their policy track.
+    CommunistPolicyTrack,
+    /// Hitler was elected chancellor
+    HitlerChancellor,
+    /// Hitler was executed
+    HitlerExecuted,
+    /// The Capitalist was executed
+    CapitalistExecuted,
+}
+
+impl ToString for Outcome {
+    fn to_string(&self) -> String {
+        match self {
+            Outcome::LiberalPolicyTrack => "LiberalPolicyTrack",
+            Outcome::FascistPolicyTrack => "FascistPolicyTrack",
+            Outcome::CommunistPolicyTrack => "CommunistPolicyTrack",
+            Outcome::HitlerChancellor => "HitlerChancellor",
+            Outcome::HitlerExecuted => "HitlerExecuted",
+            Outcome::CapitalistExecuted => "CapitalistExecuted",
+        }
+        .to_string()
+    }
+}
+
+pub async fn sync_game_stats(db: sled::Db) {
+    let client = match connect_pg().await {
+        Ok(client) => client,
+        Err(err) => return log::error!("Could not connect to PostgresQL: {:?}", err),
+    };
+
+    let sql = "INSERT INTO game (id, code, started, finished, players, outcome)
+        VALUES ($1, $2, $3, $4, $5, $6)
+        ON CONFLICT DO NOTHING;";
+    let Ok(insert) = client.prepare(sql).await else {
+        return log::error!("Could not create prepared statement");
+    };
+
+    let Ok(db) = db.open_tree("archive") else {
+        return log::error!("Could not open archive database");
+    };
+
+    log::info!("Writing game statistics to PostgresQL.");
+    loop {
+        tokio::time::sleep(Duration::from_secs(1)).await;
+
+        if client.is_closed() {
+            log::error!("Connected to PostgresQL closed.");
+            return;
+        }
+
+        let Some(entry) = db.iter().flat_map(|e| e.ok().and_then(read_row)).next() else {
+            continue;
+        };
+
+        if let Err(err) = write_row(&client, &insert, entry.0, entry.1).await {
+            log::error!("Could not write row: {:?}", err);
+            continue;
+        }
+
+        log::info!("Archived game {} to PostgresQL", entry.0);
+        db.remove(entry.0.to_be_bytes()).ok();
+    }
+}
+
+async fn connect_pg() -> Result<Client, Box<dyn Error>> {
+    let host = std::env::var("PG_HOST")?;
+    let user = std::env::var("PG_USER")?;
+    let password = std::env::var("PG_PASSWORD")?;
+    let dbname = std::env::var("PG_DBNAME")?;
+
+    let (client, connection) = tokio_postgres::Config::new()
+        .host(&host)
+        .user(&user)
+        .password(&password)
+        .dbname(&dbname)
+        .connect(NoTls)
+        .await?;
 
     tokio::spawn(async move {
         if let Err(e) = connection.await {
@@ -14,27 +104,52 @@ pub async fn foo() -> Result<(), Box<dyn Error>> {
         }
     });
 
-    let insert = client
-        .prepare(
-            "INSERT INTO game (id, code, started, finished, players, outcome)
-        VALUES ($1, $2, $3, $4, $5, $6)
-        ON CONFLICT DO NOTHING;",
-        )
-        .await?;
+    Ok(client)
+}
 
-    client
-        .execute(
-            &insert,
-            &[
-                &3i64,
-                &"ABCD",
-                &std::time::SystemTime::now(),
-                &std::time::SystemTime::now(),
-                &["Alex", "Corey", "Aidan", "Brodie"].as_slice(),
-                &"Fake",
-            ],
-        )
-        .await?;
+fn read_row(entry: (IVec, IVec)) -> Option<(i64, GameStats)> {
+    let key = u64::from_be_bytes(entry.0.as_ref().try_into().ok()?) as i64;
+    // let game = serde_json::from_slice::<GameStats>(&entry.1).ok()?;
 
+    let json = serde_json::from_slice::<Value>(&entry.1).ok()?;
+    let dt = |v: &Value| v.as_str().map(DateTime::parse_from_rfc3339);
+    let winner = json["outcome"]["winner"].as_str()?;
+    let condition = json["outcome"]["condition"].as_str()?;
+    let game = GameStats {
+        id: json["game_id"].as_str()?.to_string(),
+        started: dt(&json["started"])?.ok()?.into(),
+        finished: dt(&json["finished"])?.ok()?.into(),
+        players: json["players"]
+            .as_array()?
+            .iter()
+            .filter_map(|s| Some(s.as_str()?.to_string()))
+            .collect(),
+        outcome: match (winner, condition) {
+            ("Liberal", "legislative") => Outcome::LiberalPolicyTrack,
+            ("Liberal", "hitler") => Outcome::HitlerExecuted,
+            ("Fascist", "legislative") => Outcome::FascistPolicyTrack,
+            ("Fascist", "hitler") => Outcome::HitlerChancellor,
+            _ => return None,
+        },
+    };
+
+    Some((key, game))
+}
+
+async fn write_row(
+    client: &Client,
+    stmt: &Statement,
+    key: i64,
+    game: GameStats,
+) -> Result<(), Box<dyn Error>> {
+    let args: [&(dyn ToSql + Sync); 6] = [
+        &key,
+        &game.id.as_str(),
+        &game.started,
+        &game.finished,
+        &game.players,
+        &game.outcome.to_string(),
+    ];
+    client.execute(stmt, &args).await?;
     Ok(())
 }
